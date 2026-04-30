7fca04565e3690039f8186375dbff61005f9c140
chiuh <chiuh@ZOEPC>
2026-04-30 23:47:17 +0800

    加入專案檔案。

---
+++ BeepPlayer/frmBeepPlayer.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace BeepPlayer
+{
+    partial class frmBeepPlayer
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.palMain = new System.Windows.Forms.Panel();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.palMain.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // palMain
+            // 
+            this.palMain.Controls.Add(this.btn8);
+            this.palMain.Controls.Add(this.btn7);
+            this.palMain.Controls.Add(this.btn6);
+            this.palMain.Controls.Add(this.btn5);
+            this.palMain.Controls.Add(this.btn4);
+            this.palMain.Controls.Add(this.btn3);
+            this.palMain.Controls.Add(this.btn2);
+            this.palMain.Controls.Add(this.btn1);
+            this.palMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.palMain.Location = new System.Drawing.Point(0, 0);
+            this.palMain.Name = "palMain";
+            this.palMain.Size = new System.Drawing.Size(970, 215);
+            this.palMain.TabIndex = 0;
+            // 
+            // btn1
+            // 
+            this.btn1.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btn1.Location = new System.Drawing.Point(25, 48);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(110, 146);
+            this.btn1.TabIndex = 0;
+            this.btn1.Text = "Do";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btn2.Location = new System.Drawing.Point(141, 48);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(110, 146);
+            this.btn2.TabIndex = 1;
+            this.btn2.Text = "Re";
+            this.btn2.UseVisualStyleBackColor = true;
+            // 
+            // btn3
+            // 
+            this.btn3.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btn3.Location = new System.Drawing.Point(257, 48);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(110, 146);
+            this.btn3.TabIndex = 2;
+            this.btn3.Text = "Mi";
+            this.btn3.UseVisualStyleBackColor = true;
+            // 
+            // btn4
+            // 
+            this.btn4.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btn4.Location = new System.Drawing.Point(373, 48);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(110, 146);
+            this.btn4.TabIndex = 3;
+            this.btn4.Text = "Fa";
+            this.btn4.UseVisualStyleBackColor = true;
+            // 
+            // btn5
+            // 
+            this.btn5.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btn5.Location = new System.Drawing.Point(489, 48);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(110, 146);
+            this.btn5.TabIndex = 4;
+            this.btn5.Text = "Sol";
+            this.btn5.UseVisualStyleBackColor = true;
+            // 
+            // btn6
+            // 
+            this.btn6.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btn6.Location = new System.Drawing.Point(605, 48);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(110, 146);
+            this.btn6.TabIndex = 5;
+            this.btn6.Text = "La";
+            this.btn6.UseVisualStyleBackColor = true;
+            // 
+            // btn7
+            // 
+            this.btn7.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btn7.Location = new System.Drawing.Point(721, 48);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(110, 146);
+            this.btn7.TabIndex = 6;
+            this.btn7.Text = "Si";
+            this.btn7.UseVisualStyleBackColor = true;
+            // 
+            // btn8
+            // 
+            this.btn8.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btn8.ForeColor = System.Drawing.Color.Brown;
+            this.btn8.Location = new System.Drawing.Point(837, 48);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(110, 146);
+            this.btn8.TabIndex = 7;
+            this.btn8.Text = "Do";
+            this.btn8.UseVisualStyleBackColor = true;
+            // 
+            // frmBeepPlayer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(970, 215);
+            this.Controls.Add(this.palMain);
+            this.Name = "frmBeepPlayer";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmBeepPlayer_FormClosing);
+            this.Load += new System.EventHandler(this.frmBeepPlayer_Load);
+            this.SizeChanged += new System.EventHandler(this.frmBeepPlayer_SizeChanged);
+            this.palMain.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel palMain;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn4;
+    }
+}
+
